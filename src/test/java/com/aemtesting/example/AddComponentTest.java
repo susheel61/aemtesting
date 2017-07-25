9fa914f3b467ef8f7c5ff428041488886f8353e1
Susheel Singh <susheels@techaspect.com>
2017-07-26 01:23:36 +0530

    clean up

--- src/test/java/com/aemtesting/example/AddComponentTest.java
+++ src/test/java/com/aemtesting/example/AddComponentTest.java
@@ -9,9 +9,7 @@ import org.hamcrest.CoreMatchers;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 
 import com.aemtesting.GuiceModule;
 import com.aemtesting.custom.EditToolbarUtil;
@@ -22,10 +20,8 @@ import com.cognifide.qa.bb.aem.AemLogin;
 import com.cognifide.qa.bb.aem.ui.AemContentFinder;
 import com.cognifide.qa.bb.aem.ui.menu.MenuOption;
 import com.cognifide.qa.bb.aem.ui.parsys.AemParsys;
-import com.cognifide.qa.bb.constants.Timeouts;
 import com.cognifide.qa.bb.junit.Modules;
 import com.cognifide.qa.bb.junit.TestRunner;
-import com.cognifide.qa.bb.provider.selenium.BobcatWait;
 import com.google.inject.Inject;
 
 @RunWith(TestRunner.class)
@@ -40,9 +36,6 @@ public class AddComponentTest {
 
 	@Inject
 	private AemContentFinder contentFinder;
-	
-	@Inject
-	private BobcatWait bobcatWait;
 
 	@Inject
 	private WebDriver webdriver;
@@ -59,28 +52,27 @@ public class AddComponentTest {
 
 	@Test
 	public void testAddingAndEditingImageComponent() {
-		 ImageComponent imageComponent = parsys.insertComponent(ImageComponent.class);
-		 assertTrue(whatIsBobcatPage.isDisplayed());
-		 EditToolbarUtil.click(imageComponent, webdriver,MenuOption.EDIT);
-		 contentFinder.clickTab("Images");
-		 contentFinder.search("icon-FrontEnd-shield");
-		 List<String> results = contentFinder.getResults();
-		 if (results.size() > 0) {
-		 imageComponent.insert(contentFinder.getElementByIndex(0));
-		 assertTrue("Image not found",
-		 results.get(0).contains(imageComponent.getImgAttribute("title")));
-		 imageComponent.getDialog().clickTab("Advanced");
-		 imageComponent.getTitle().setValue("This is a heading for image component");
-		 imageComponent.getAltText().setValue("This is an alt text");
-		 imageComponent.getLinkTo().setValue("/content/geometrixx-media/en");
-		 imageComponent.getDescription().setValue("Sed hendrerit condimentum erat, non venenatis leo pulvinar ac.");
-		 imageComponent.getDialog().ok();
-		 }
+		ImageComponent imageComponent = parsys.insertComponent(ImageComponent.class);
+		assertTrue(whatIsBobcatPage.isDisplayed());
+		EditToolbarUtil.click(imageComponent, webdriver, MenuOption.EDIT);
+		contentFinder.clickTab("Images");
+		contentFinder.search("icon-FrontEnd-shield");
+		List<String> results = contentFinder.getResults();
+		if (results.size() > 0) {
+			imageComponent.insert(contentFinder.getElementByIndex(0));
+			assertTrue("Image not found", results.get(0).contains(imageComponent.getImgAttribute("title")));
+			imageComponent.getDialog().clickTab("Advanced");
+			imageComponent.getTitle().setValue("This is a heading for image component");
+			imageComponent.getAltText().setValue("This is an alt text");
+			imageComponent.getLinkTo().setValue("/content/geometrixx-media/en");
+			imageComponent.getDescription().setValue("Sed hendrerit condimentum erat, non venenatis leo pulvinar ac.");
+			imageComponent.getDialog().ok();
+		}
 		assertThat(imageComponent, CoreMatchers.notNullValue());
 
 		PopularArticles popularArticles = parsys.insertComponent(PopularArticles.class);
 		assertTrue(whatIsBobcatPage.isDisplayed());
-		System.out.println("Page Reloaded");	
+		System.out.println("Page Reloaded");
 		EditToolbarUtil.click(popularArticles, webdriver, MenuOption.EDIT);
 		popularArticles.getTopLevelPage().setValue("/content/geometrixx-media/en");
 		popularArticles.getDialog().clickTab("Defaults");
